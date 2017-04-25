7dc72449b830031afd0d7da40b93eaf36ef4a71b
SantiagoSolis <Santiago@DESKTOP-VS0357J>
2017-04-24 23:45:16 -0300

    hice cambios en tal lado

--- PAVTPI/PAVTPI/AbmDependencia.Designer.vb
+++ PAVTPI/PAVTPI/AbmDependencia.Designer.vb
@@ -22,8 +22,16 @@ Partial Class AbmDependencia
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'AbmDependencia
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Name = "AbmDependencia"
         Me.Text = "AbmDependencia"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
