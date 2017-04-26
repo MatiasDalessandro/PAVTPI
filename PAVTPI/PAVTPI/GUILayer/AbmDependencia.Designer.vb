e1e1073a1824e5f58aa1e3fee3c3df8149fd645a
SantiagoSolis <Santiago@DESKTOP-VS0357J>
2017-04-25 23:16:38 -0300

    dependencia

--- PAVTPI/PAVTPI/GUILayer/AbmDependencia.Designer.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmDependencia.Designer.vb
@@ -22,16 +22,134 @@ Partial Class AbmDependencia
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.dgv_datos_dependencia = New System.Windows.Forms.DataGridView()
+        Me.nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nro_cuenta = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.saldo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.estado = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.lbl_nombre = New System.Windows.Forms.Label()
+        Me.lbl_descripcion = New System.Windows.Forms.Label()
+        Me.lbl_estado = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        CType(Me.dgv_datos_dependencia, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'dgv_datos_dependencia
+        '
+        Me.dgv_datos_dependencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_datos_dependencia.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.nombre, Me.descripcion, Me.nro_cuenta, Me.saldo, Me.estado})
+        Me.dgv_datos_dependencia.Location = New System.Drawing.Point(12, 28)
+        Me.dgv_datos_dependencia.Name = "dgv_datos_dependencia"
+        Me.dgv_datos_dependencia.Size = New System.Drawing.Size(545, 211)
+        Me.dgv_datos_dependencia.TabIndex = 0
+        '
+        'nombre
+        '
+        Me.nombre.HeaderText = "Nombre"
+        Me.nombre.Name = "nombre"
+        '
+        'descripcion
+        '
+        Me.descripcion.HeaderText = "Descripción"
+        Me.descripcion.Name = "descripcion"
+        '
+        'nro_cuenta
+        '
+        Me.nro_cuenta.HeaderText = "Número de cuenta"
+        Me.nro_cuenta.Name = "nro_cuenta"
+        '
+        'saldo
+        '
+        Me.saldo.HeaderText = "Saldo"
+        Me.saldo.Name = "saldo"
+        '
+        'estado
+        '
+        Me.estado.HeaderText = "Estado"
+        Me.estado.Name = "estado"
+        '
+        'lbl_nombre
+        '
+        Me.lbl_nombre.AutoSize = True
+        Me.lbl_nombre.Location = New System.Drawing.Point(16, 293)
+        Me.lbl_nombre.Name = "lbl_nombre"
+        Me.lbl_nombre.Size = New System.Drawing.Size(44, 13)
+        Me.lbl_nombre.TabIndex = 1
+        Me.lbl_nombre.Text = "Nombre"
+        '
+        'lbl_descripcion
+        '
+        Me.lbl_descripcion.AutoSize = True
+        Me.lbl_descripcion.Location = New System.Drawing.Point(16, 319)
+        Me.lbl_descripcion.Name = "lbl_descripcion"
+        Me.lbl_descripcion.Size = New System.Drawing.Size(63, 13)
+        Me.lbl_descripcion.TabIndex = 2
+        Me.lbl_descripcion.Text = "Descripción"
+        '
+        'lbl_estado
+        '
+        Me.lbl_estado.AutoSize = True
+        Me.lbl_estado.Location = New System.Drawing.Point(16, 346)
+        Me.lbl_estado.Name = "lbl_estado"
+        Me.lbl_estado.Size = New System.Drawing.Size(40, 13)
+        Me.lbl_estado.TabIndex = 5
+        Me.lbl_estado.Text = "Estado"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(121, 338)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(192, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(121, 286)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(121, 312)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(192, 20)
+        Me.TextBox2.TabIndex = 9
         '
         'AbmDependencia
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(571, 518)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.lbl_estado)
+        Me.Controls.Add(Me.lbl_descripcion)
+        Me.Controls.Add(Me.lbl_nombre)
+        Me.Controls.Add(Me.dgv_datos_dependencia)
         Me.Name = "AbmDependencia"
         Me.Text = "AbmDependencia"
+        CType(Me.dgv_datos_dependencia, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents dgv_datos_dependencia As DataGridView
+    Friend WithEvents nombre As DataGridViewTextBoxColumn
+    Friend WithEvents descripcion As DataGridViewTextBoxColumn
+    Friend WithEvents nro_cuenta As DataGridViewTextBoxColumn
+    Friend WithEvents saldo As DataGridViewTextBoxColumn
+    Friend WithEvents estado As DataGridViewTextBoxColumn
+    Friend WithEvents lbl_nombre As Label
+    Friend WithEvents lbl_descripcion As Label
+    Friend WithEvents lbl_estado As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- PAVTPI/PAVTPI/GUILayer/AbmDependencia.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmDependencia.vb
@@ -1,5 +1,32 @@
 ﻿Public Class AbmDependencia
+    Dim cadena_conexion As String = "Provider=SQLNCLI11;Data Source=DESKTOP-VS0357J\SQLEXPRESS;Integrated Security=SSPI"
     Private Sub AbmDependencia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'holaaaaaa chupa pija
+        cargarGrilla()
     End Sub
+    Private Function ejecuto_sql(ByVal consulta As String)
+        Dim conexion As New OleDb.OleDbConnection
+        Dim cmd As New OleDb.OleDbCommand
+        Dim tabla As New DataTable
+
+        conexion.ConnectionString = cadena_conexion
+        conexion.Open()
+        cmd.Connection = conexion
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = consulta
+        tabla.Load(cmd.ExecuteReader)
+        conexion.Close()
+        Return tabla
+    End Function
+    Private Sub cargarGrilla()
+        Dim sql As String = "SELECT * FROM Dependencia"
+        Dim tabla As DataTable = ejecuto_sql(sql)
+        Dim c As Integer = 0
+        For c = 0 To tabla.Rows.Count - 1
+            Me.dgv_datos_dependencia.Rows.Add()
+            Me.dgv_datos_dependencia.Rows(c).Cells("c_id_articulo").Value = tabla.Rows(c)(0)
+            Me.dgv_datos_dependencia.Rows(c).Cells("c_nombre_articulo").Value = tabla.Rows(c)(1)
+            Me.dgv_datos_dependencia.Rows(c).Cells("c_precio_articulo").Value = tabla.Rows(c)(2)
+        Next
+    End Sub
+
 End Class
