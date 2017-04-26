d696d5e492721b81d83b7004592e4f1d7107f28c
levicastro36 <levicastro36@gmail.com>
2017-04-26 19:03:11 -0300

    abm provincias

--- PAVTPI/PAVTPI/GUILayer/AbmProvincia.Designer.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmProvincia.Designer.vb
@@ -22,8 +22,124 @@ Partial Class AbmProvincia
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txt_idProvincia = New System.Windows.Forms.TextBox()
+        Me.txt_nomProvincia = New System.Windows.Forms.TextBox()
+        Me.btn_nuevo = New System.Windows.Forms.Button()
+        Me.btn_guardar = New System.Windows.Forms.Button()
+        Me.btn_salir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_buscar = New System.Windows.Forms.Button()
+        Me.btn_editar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt_idProvincia
+        '
+        Me.txt_idProvincia.Location = New System.Drawing.Point(108, 17)
+        Me.txt_idProvincia.Name = "txt_idProvincia"
+        Me.txt_idProvincia.Size = New System.Drawing.Size(36, 20)
+        Me.txt_idProvincia.TabIndex = 0
+        '
+        'txt_nomProvincia
+        '
+        Me.txt_nomProvincia.Location = New System.Drawing.Point(108, 56)
+        Me.txt_nomProvincia.Name = "txt_nomProvincia"
+        Me.txt_nomProvincia.Size = New System.Drawing.Size(141, 20)
+        Me.txt_nomProvincia.TabIndex = 1
+        '
+        'btn_nuevo
+        '
+        Me.btn_nuevo.Location = New System.Drawing.Point(12, 93)
+        Me.btn_nuevo.Name = "btn_nuevo"
+        Me.btn_nuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btn_nuevo.TabIndex = 2
+        Me.btn_nuevo.Text = "Nuevo"
+        Me.btn_nuevo.UseVisualStyleBackColor = True
+        '
+        'btn_guardar
+        '
+        Me.btn_guardar.Location = New System.Drawing.Point(93, 93)
+        Me.btn_guardar.Name = "btn_guardar"
+        Me.btn_guardar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_guardar.TabIndex = 3
+        Me.btn_guardar.Text = "Guardar"
+        Me.btn_guardar.UseVisualStyleBackColor = True
+        '
+        'btn_salir
+        '
+        Me.btn_salir.Location = New System.Drawing.Point(174, 93)
+        Me.btn_salir.Name = "btn_salir"
+        Me.btn_salir.Size = New System.Drawing.Size(75, 23)
+        Me.btn_salir.TabIndex = 4
+        Me.btn_salir.Text = "Salir"
+        Me.btn_salir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Id Provincia"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Nombre Provincia"
+        '
+        'btn_buscar
+        '
+        Me.btn_buscar.ImageAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btn_buscar.Location = New System.Drawing.Point(223, 12)
+        Me.btn_buscar.Name = "btn_buscar"
+        Me.btn_buscar.Size = New System.Drawing.Size(28, 30)
+        Me.btn_buscar.TabIndex = 8
+        Me.btn_buscar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.btn_buscar.UseVisualStyleBackColor = True
+        '
+        'btn_editar
+        '
+        Me.btn_editar.ImageAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btn_editar.Location = New System.Drawing.Point(189, 12)
+        Me.btn_editar.Name = "btn_editar"
+        Me.btn_editar.Size = New System.Drawing.Size(28, 30)
+        Me.btn_editar.TabIndex = 7
+        Me.btn_editar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.btn_editar.UseVisualStyleBackColor = True
+        '
+        'AbmProvincia
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(261, 132)
+        Me.Controls.Add(Me.btn_buscar)
+        Me.Controls.Add(Me.btn_editar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_salir)
+        Me.Controls.Add(Me.btn_guardar)
+        Me.Controls.Add(Me.btn_nuevo)
+        Me.Controls.Add(Me.txt_nomProvincia)
+        Me.Controls.Add(Me.txt_idProvincia)
+        Me.Name = "AbmProvincia"
         Me.Text = "AbmProvincia"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txt_idProvincia As TextBox
+    Friend WithEvents txt_nomProvincia As TextBox
+    Friend WithEvents btn_nuevo As Button
+    Friend WithEvents btn_guardar As Button
+    Friend WithEvents btn_salir As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn_editar As Button
+    Friend WithEvents btn_buscar As Button
 End Class
--- PAVTPI/PAVTPI/GUILayer/AbmProvincia.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmProvincia.vb
@@ -1,3 +1,6 @@
 ﻿Public Class AbmProvincia
-
+    Private Sub btn_nuevo_Click(sender As Object, e As EventArgs) Handles btn_nuevo.Click
+        txt_idProvincia.Text = Nothing
+        txt_nomProvincia.Text = Nothing
+    End Sub
 End Class
