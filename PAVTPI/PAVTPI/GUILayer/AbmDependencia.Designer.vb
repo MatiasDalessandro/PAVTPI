6aaaca5e31fa6cd21bed8e4ce61a34c999412b7a
SantiagoSolis <Santiago@DESKTOP-VS0357J>
2017-04-26 02:29:45 -0300

    abm terminado, sin validaciones  y muy bugueable

--- PAVTPI/PAVTPI/GUILayer/AbmDependencia.Designer.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmDependencia.Designer.vb
@@ -28,20 +28,27 @@ Partial Class AbmDependencia
         Me.nro_cuenta = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.saldo = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.estado = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.id = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.lbl_nombre = New System.Windows.Forms.Label()
         Me.lbl_descripcion = New System.Windows.Forms.Label()
         Me.lbl_estado = New System.Windows.Forms.Label()
         Me.cmb_estado = New System.Windows.Forms.ComboBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txt_nombre = New System.Windows.Forms.TextBox()
+        Me.txt_descripcion = New System.Windows.Forms.TextBox()
+        Me.lbl_cta_corriente = New System.Windows.Forms.Label()
+        Me.txt_nro = New System.Windows.Forms.TextBox()
+        Me.btn_nuevo = New System.Windows.Forms.Button()
+        Me.btn_guardar = New System.Windows.Forms.Button()
+        Me.btn_borrar = New System.Windows.Forms.Button()
+        Me.btn_salir = New System.Windows.Forms.Button()
         CType(Me.dgv_datos_dependencia, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgv_datos_dependencia
         '
         Me.dgv_datos_dependencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgv_datos_dependencia.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.nombre, Me.descripcion, Me.nro_cuenta, Me.saldo, Me.estado})
-        Me.dgv_datos_dependencia.Location = New System.Drawing.Point(12, 28)
+        Me.dgv_datos_dependencia.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.nombre, Me.descripcion, Me.nro_cuenta, Me.saldo, Me.estado, Me.id})
+        Me.dgv_datos_dependencia.Location = New System.Drawing.Point(12, 22)
         Me.dgv_datos_dependencia.Name = "dgv_datos_dependencia"
         Me.dgv_datos_dependencia.Size = New System.Drawing.Size(545, 211)
         Me.dgv_datos_dependencia.TabIndex = 0
@@ -70,6 +77,11 @@ Partial Class AbmDependencia
         '
         Me.estado.HeaderText = "Estado"
         Me.estado.Name = "estado"
+        '
+        'id
+        '
+        Me.id.HeaderText = "id"
+        Me.id.Name = "id"
         '
         'lbl_nombre
         '
@@ -101,32 +113,92 @@ Partial Class AbmDependencia
         'cmb_estado
         '
         Me.cmb_estado.FormattingEnabled = True
-        Me.cmb_estado.Location = New System.Drawing.Point(121, 338)
+        Me.cmb_estado.Location = New System.Drawing.Point(182, 338)
         Me.cmb_estado.Name = "cmb_estado"
         Me.cmb_estado.Size = New System.Drawing.Size(192, 21)
         Me.cmb_estado.TabIndex = 7
         '
-        'TextBox1
+        'txt_nombre
         '
-        Me.TextBox1.Location = New System.Drawing.Point(121, 286)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(192, 20)
-        Me.TextBox1.TabIndex = 8
+        Me.txt_nombre.Location = New System.Drawing.Point(182, 286)
+        Me.txt_nombre.Name = "txt_nombre"
+        Me.txt_nombre.Size = New System.Drawing.Size(192, 20)
+        Me.txt_nombre.TabIndex = 8
         '
-        'TextBox2
+        'txt_descripcion
         '
-        Me.TextBox2.Location = New System.Drawing.Point(121, 312)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(192, 20)
-        Me.TextBox2.TabIndex = 9
+        Me.txt_descripcion.Location = New System.Drawing.Point(182, 312)
+        Me.txt_descripcion.Name = "txt_descripcion"
+        Me.txt_descripcion.Size = New System.Drawing.Size(192, 20)
+        Me.txt_descripcion.TabIndex = 9
+        '
+        'lbl_cta_corriente
+        '
+        Me.lbl_cta_corriente.AutoSize = True
+        Me.lbl_cta_corriente.Location = New System.Drawing.Point(16, 371)
+        Me.lbl_cta_corriente.Name = "lbl_cta_corriente"
+        Me.lbl_cta_corriente.Size = New System.Drawing.Size(141, 13)
+        Me.lbl_cta_corriente.TabIndex = 10
+        Me.lbl_cta_corriente.Text = "Número de Cuenta Corriente"
+        '
+        'txt_nro
+        '
+        Me.txt_nro.Location = New System.Drawing.Point(182, 368)
+        Me.txt_nro.Name = "txt_nro"
+        Me.txt_nro.ReadOnly = True
+        Me.txt_nro.Size = New System.Drawing.Size(192, 20)
+        Me.txt_nro.TabIndex = 11
+        Me.txt_nro.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn_nuevo
+        '
+        Me.btn_nuevo.Location = New System.Drawing.Point(19, 459)
+        Me.btn_nuevo.Name = "btn_nuevo"
+        Me.btn_nuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btn_nuevo.TabIndex = 12
+        Me.btn_nuevo.Text = "Nuevo"
+        Me.btn_nuevo.UseVisualStyleBackColor = True
+        '
+        'btn_guardar
+        '
+        Me.btn_guardar.Location = New System.Drawing.Point(100, 459)
+        Me.btn_guardar.Name = "btn_guardar"
+        Me.btn_guardar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_guardar.TabIndex = 13
+        Me.btn_guardar.Text = "Guardar"
+        Me.btn_guardar.UseVisualStyleBackColor = True
+        '
+        'btn_borrar
+        '
+        Me.btn_borrar.Location = New System.Drawing.Point(182, 459)
+        Me.btn_borrar.Name = "btn_borrar"
+        Me.btn_borrar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_borrar.TabIndex = 14
+        Me.btn_borrar.Text = "Borrar"
+        Me.btn_borrar.UseVisualStyleBackColor = True
+        '
+        'btn_salir
+        '
+        Me.btn_salir.Location = New System.Drawing.Point(482, 459)
+        Me.btn_salir.Name = "btn_salir"
+        Me.btn_salir.Size = New System.Drawing.Size(75, 23)
+        Me.btn_salir.TabIndex = 15
+        Me.btn_salir.Text = "Salir"
+        Me.btn_salir.UseVisualStyleBackColor = True
         '
         'AbmDependencia
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(571, 518)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btn_salir)
+        Me.Controls.Add(Me.btn_borrar)
+        Me.Controls.Add(Me.btn_guardar)
+        Me.Controls.Add(Me.btn_nuevo)
+        Me.Controls.Add(Me.txt_nro)
+        Me.Controls.Add(Me.lbl_cta_corriente)
+        Me.Controls.Add(Me.txt_descripcion)
+        Me.Controls.Add(Me.txt_nombre)
         Me.Controls.Add(Me.cmb_estado)
         Me.Controls.Add(Me.lbl_estado)
         Me.Controls.Add(Me.lbl_descripcion)
@@ -141,15 +213,22 @@ Partial Class AbmDependencia
     End Sub
 
     Friend WithEvents dgv_datos_dependencia As DataGridView
+    Friend WithEvents lbl_nombre As Label
+    Friend WithEvents lbl_descripcion As Label
+    Friend WithEvents lbl_estado As Label
+    Friend WithEvents cmb_estado As ComboBox
+    Friend WithEvents txt_nombre As TextBox
+    Friend WithEvents txt_descripcion As TextBox
+    Friend WithEvents lbl_cta_corriente As Label
+    Friend WithEvents txt_nro As TextBox
+    Friend WithEvents btn_nuevo As Button
+    Friend WithEvents btn_guardar As Button
     Friend WithEvents nombre As DataGridViewTextBoxColumn
     Friend WithEvents descripcion As DataGridViewTextBoxColumn
     Friend WithEvents nro_cuenta As DataGridViewTextBoxColumn
     Friend WithEvents saldo As DataGridViewTextBoxColumn
     Friend WithEvents estado As DataGridViewTextBoxColumn
-    Friend WithEvents lbl_nombre As Label
-    Friend WithEvents lbl_descripcion As Label
-    Friend WithEvents lbl_estado As Label
-    Friend WithEvents cmb_estado As ComboBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents id As DataGridViewTextBoxColumn
+    Friend WithEvents btn_borrar As Button
+    Friend WithEvents btn_salir As Button
 End Class
--- PAVTPI/PAVTPI/GUILayer/AbmDependencia.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmDependencia.vb
@@ -1,5 +1,11 @@
 ﻿Public Class AbmDependencia
     Dim cadena_conexion As String = "Provider=SQLNCLI11;Data Source=DESKTOP-VS0357J\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=PAV-TPI"
+    Enum estado_grabacion
+        insertar
+        modificar
+    End Enum
+    Dim condicion_grabacion As estado_grabacion = estado_grabacion.insertar
+
     Private Sub AbmDependencia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cargarGrilla()
         cargar_combo()
@@ -14,21 +20,25 @@
         cmd.Connection = conexion
         cmd.CommandType = CommandType.Text
         cmd.CommandText = consulta
+        cmd.CommandText = consulta
         tabla.Load(cmd.ExecuteReader())
         conexion.Close()
         Return tabla
     End Function
     Private Sub cargarGrilla()
-        Dim sql As String = "SELECT Nombre,Descripcion,NroCuentaCorriente,Saldo,Estado from Dependencia"
+
+        Dim sql As String = "SELECT IdDependencia,Nombre,Descripcion,NroCuentaCorriente,Saldo,Estado from Dependencia"
         Dim tabla As DataTable = ejecuto_sql(sql)
+        dgv_datos_dependencia.Rows.Clear()
         Dim c As Integer = 0
         For c = 0 To tabla.Rows.Count - 1
             Me.dgv_datos_dependencia.Rows.Add()
             Me.dgv_datos_dependencia.Rows(c).Cells(0).Value = tabla.Rows(c)("nombre")
-            Me.dgv_datos_dependencia.Rows(c).Cells(1).Value = tabla.Rows(c)(1)
-            Me.dgv_datos_dependencia.Rows(c).Cells(2).Value = tabla.Rows(c)(2)
-            Me.dgv_datos_dependencia.Rows(c).Cells(3).Value = tabla.Rows(c)(3)
-            Me.dgv_datos_dependencia.Rows(c).Cells(4).Value = validar_estado(tabla.Rows(c)(4))
+            Me.dgv_datos_dependencia.Rows(c).Cells(1).Value = tabla.Rows(c)(2)
+            Me.dgv_datos_dependencia.Rows(c).Cells(2).Value = tabla.Rows(c)(3)
+            Me.dgv_datos_dependencia.Rows(c).Cells(3).Value = tabla.Rows(c)(4)
+            Me.dgv_datos_dependencia.Rows(c).Cells(4).Value = validar_estado(tabla.Rows(c)(5))
+            Me.dgv_datos_dependencia.Rows(c).Cells(5).Value = tabla.Rows(c)(0)
         Next
     End Sub
     Private Function validar_estado(ByVal estado As Integer)
@@ -38,10 +48,19 @@
         End If
         Return "Deshabilitado"
     End Function
+    Private Function validar_estado_r(ByVal estado As String)
+        If estado = "Habilitado" Then
+            Return 1
+            Exit Function
+
+        End If
+        Return 0
+    End Function
     Private Sub cargar_combo()
         Dim combo As ComboBox = Me.cmb_estado
         combo.Items.Add("Habilitado")
         combo.Items.Add("Deshabilitado")
+
     End Sub
     Private Function leo_tabla(ByVal sql As String) As DataTable
         Dim tabla As DataTable
@@ -49,4 +68,102 @@
         Return tabla
     End Function
 
+    Private Sub btn_nuevo_Click(sender As Object, e As EventArgs) Handles btn_nuevo.Click
+        For Each obj As Control In Me.Controls
+            If obj.GetType.Name = "TextBox" Or obj.GetType.Name = "MaskedTextBox" Then
+                obj.Text = ""
+            End If
+            If obj.GetType.Name = "ComboBox" Then
+                Dim o As ComboBox = obj
+                o.SelectedValue = -1
+            End If
+        Next
+        Me.txt_nombre.Enabled = True
+        Me.txt_descripcion.Enabled = True
+        Me.txt_nro.ReadOnly = False
+        Dim valor As Integer = dgv_datos_dependencia.Rows.Count
+        Dim nro As Integer = dgv_datos_dependencia.Rows(valor - 1).Cells(5).Value
+        Me.txt_nro.Text = valor
+        Me.condicion_grabacion = estado_grabacion.insertar
+        Me.txt_nombre.Focus()
+
+
+
+    End Sub
+    Private Sub validar_dependencias()
+        Dim tabla As DataTable
+        Dim sql As String = 
+    End Sub
+    Private Sub grabar_borrar(sql As String)
+        Dim conexion As New OleDb.OleDbConnection
+        Dim cmd As New OleDb.OleDbCommand
+        conexion.ConnectionString = cadena_conexion
+        conexion.Open()
+        cmd.Connection = conexion
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = sql
+        cmd.ExecuteNonQuery()
+        conexion.Close()
+    End Sub
+    Private Sub insertar()
+        Dim nro As Integer = Me.txt_nro.Text
+        Dim estado As Integer = validar_estado_r(Me.cmb_estado.SelectedValue)
+        Dim sql As String = ""
+        sql &= "insert into [PAV-TPI].dbo.Dependencia (Nombre,Descripcion,NroCuentaCorriente,Estado) VALUES ('"
+        sql &= Me.txt_nombre.Text & "', '" & Me.txt_descripcion.Text & "'," & nro & "," & estado & ")"
+        grabar_borrar(sql)
+
+    End Sub
+    Private Sub modificar()
+        Dim id As Integer = dgv_datos_dependencia.CurrentRow.Cells(5).Value
+        Dim sql As String = "UPDATE [PAV-TPI].dbo.Dependencia set nombre='" & Me.txt_nombre.Text & "',descripcion = '" & txt_descripcion.Text & "' where IdDependencia = "
+        sql &= id
+        grabar_borrar(sql)
+
+
+    End Sub
+    Private Sub btn_guardar_Click(sender As Object, e As EventArgs) Handles btn_guardar.Click
+        If condicion_grabacion = estado_grabacion.insertar Then
+            insertar()
+        Else
+            modificar()
+        End If
+        cargarGrilla()
+        MsgBox("Se cargo correctamente.")
+    End Sub
+
+    Private Sub dgv_datos_dependencia_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv_datos_dependencia.CellContentClick
+        Dim sql As String = ""
+        sql &= "SELECT * FROM [PAV-TPI].dbo.Dependencia WHERE IdDependencia = " & dgv_datos_dependencia.CurrentRow.Cells(5).Value
+        Dim tabla As DataTable = ejecuto_sql(sql)
+        If tabla.Rows.Count = 0 Then
+            MsgBox("No hay personas para recuperar")
+        End If
+        Me.txt_nombre.Text = tabla.Rows(0)("Nombre")
+        Me.txt_descripcion.Text = tabla.Rows(0)("Descripcion")
+        Me.txt_nro.Text = tabla.Rows(0)("NroCuentaCorriente")
+        Me.cmb_estado.Text = validar_estado(tabla.Rows(0)("Estado"))
+        condicion_grabacion = estado_grabacion.modificar
+
+
+    End Sub
+
+    Private Sub btn_borrar_Click(sender As Object, e As EventArgs) Handles btn_borrar.Click
+        If MessageBox.Show("Esta seguro de borrar: " + Chr(13) +
+                           "Dependencia: " & Me.dgv_datos_dependencia.CurrentRow.Cells(0).Value.ToString.Trim + Chr(10) +
+                           "Descripcion: " & Me.dgv_datos_dependencia.CurrentRow.Cells(1).Value.ToString.Trim _
+                           , "Importante" _
+                           , MessageBoxButtons.YesNo _
+                           , MessageBoxIcon.Question) = System.Windows.Forms.DialogResult.No Then
+            Exit Sub
+        End If
+        Dim sql As String = "DELETE FROM [PAV-TPI].dbo.Dependencia where IdDependencia = "
+        sql &= dgv_datos_dependencia.CurrentRow.Cells(5).Value
+        grabar_borrar(sql)
+        cargarGrilla()
+    End Sub
+
+    Private Sub btn_salir_Click(sender As Object, e As EventArgs) Handles btn_salir.Click
+        Me.Close()
+    End Sub
 End Class
