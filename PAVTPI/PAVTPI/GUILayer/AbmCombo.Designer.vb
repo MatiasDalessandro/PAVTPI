b1b5eb7dd433893d2bed31edc65874eb3d10dedd
levicastro36 <levicastro36@gmail.com>
2017-05-17 17:45:45 -0300

    dasda

--- PAVTPI/PAVTPI/GUILayer/AbmCombo.Designer.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmCombo.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class AbmCombo
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class AbmCombo
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.dgv_datos_articulos = New System.Windows.Forms.DataGridView()
         Me.c_id_articulo = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -31,19 +31,18 @@ Partial Class AbmCombo
         Me.btn_salir = New System.Windows.Forms.Button()
         Me.btn_guardar = New System.Windows.Forms.Button()
         Me.btn_eliminar = New System.Windows.Forms.Button()
-        Me.btn_nuevo = New System.Windows.Forms.Button()
-        Me.txt_precio = New System.Windows.Forms.TextBox()
         Me.txt_nombre_articulo = New System.Windows.Forms.TextBox()
         Me.txt_id_combo = New System.Windows.Forms.TextBox()
-        Me.lbl_precio = New System.Windows.Forms.Label()
         Me.lbl_nombre_combo = New System.Windows.Forms.Label()
         Me.lbl_id_combo = New System.Windows.Forms.Label()
         Me.btn_agregar_articulo = New System.Windows.Forms.Button()
         Me.btn_quitar_articulo = New System.Windows.Forms.Button()
         Me.btn_editar_combo = New System.Windows.Forms.Button()
-        Me.txt_precio_sugerido = New System.Windows.Forms.TextBox()
-        Me.lbl_precio_sugerido = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuscarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.dgv_datos_articulos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'dgv_datos_articulos
@@ -51,7 +50,7 @@ Partial Class AbmCombo
         Me.dgv_datos_articulos.AllowUserToDeleteRows = False
         Me.dgv_datos_articulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgv_datos_articulos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.c_id_articulo, Me.c_nombre_articulo, Me.c_precio_articulo, Me.Borrar})
-        Me.dgv_datos_articulos.Location = New System.Drawing.Point(27, 141)
+        Me.dgv_datos_articulos.Location = New System.Drawing.Point(27, 148)
         Me.dgv_datos_articulos.Name = "dgv_datos_articulos"
         Me.dgv_datos_articulos.ReadOnly = True
         Me.dgv_datos_articulos.Size = New System.Drawing.Size(463, 228)
@@ -89,7 +88,7 @@ Partial Class AbmCombo
         '
         'btn_buscar
         '
-        Me.btn_buscar.Location = New System.Drawing.Point(223, 444)
+        Me.btn_buscar.Location = New System.Drawing.Point(292, 33)
         Me.btn_buscar.Name = "btn_buscar"
         Me.btn_buscar.Size = New System.Drawing.Size(75, 23)
         Me.btn_buscar.TabIndex = 25
@@ -98,7 +97,7 @@ Partial Class AbmCombo
         '
         'btn_salir
         '
-        Me.btn_salir.Location = New System.Drawing.Point(415, 444)
+        Me.btn_salir.Location = New System.Drawing.Point(346, 451)
         Me.btn_salir.Name = "btn_salir"
         Me.btn_salir.Size = New System.Drawing.Size(75, 23)
         Me.btn_salir.TabIndex = 26
@@ -108,7 +107,7 @@ Partial Class AbmCombo
         'btn_guardar
         '
         Me.btn_guardar.Enabled = False
-        Me.btn_guardar.Location = New System.Drawing.Point(125, 444)
+        Me.btn_guardar.Location = New System.Drawing.Point(98, 451)
         Me.btn_guardar.Name = "btn_guardar"
         Me.btn_guardar.Size = New System.Drawing.Size(75, 23)
         Me.btn_guardar.TabIndex = 27
@@ -118,58 +117,32 @@ Partial Class AbmCombo
         'btn_eliminar
         '
         Me.btn_eliminar.Enabled = False
-        Me.btn_eliminar.Location = New System.Drawing.Point(318, 444)
+        Me.btn_eliminar.Location = New System.Drawing.Point(222, 451)
         Me.btn_eliminar.Name = "btn_eliminar"
         Me.btn_eliminar.Size = New System.Drawing.Size(75, 23)
         Me.btn_eliminar.TabIndex = 28
         Me.btn_eliminar.Text = "Eliminar"
         Me.btn_eliminar.UseVisualStyleBackColor = True
         '
-        'btn_nuevo
-        '
-        Me.btn_nuevo.Location = New System.Drawing.Point(29, 444)
-        Me.btn_nuevo.Name = "btn_nuevo"
-        Me.btn_nuevo.Size = New System.Drawing.Size(75, 23)
-        Me.btn_nuevo.TabIndex = 29
-        Me.btn_nuevo.Text = "Nuevo"
-        Me.btn_nuevo.UseVisualStyleBackColor = True
-        '
-        'txt_precio
-        '
-        Me.txt_precio.Enabled = False
-        Me.txt_precio.Location = New System.Drawing.Point(125, 100)
-        Me.txt_precio.Name = "txt_precio"
-        Me.txt_precio.Size = New System.Drawing.Size(100, 20)
-        Me.txt_precio.TabIndex = 22
-        '
         'txt_nombre_articulo
         '
         Me.txt_nombre_articulo.Enabled = False
-        Me.txt_nombre_articulo.Location = New System.Drawing.Point(125, 64)
+        Me.txt_nombre_articulo.Location = New System.Drawing.Point(125, 71)
         Me.txt_nombre_articulo.Name = "txt_nombre_articulo"
         Me.txt_nombre_articulo.Size = New System.Drawing.Size(156, 20)
         Me.txt_nombre_articulo.TabIndex = 23
         '
         'txt_id_combo
         '
-        Me.txt_id_combo.Location = New System.Drawing.Point(125, 29)
+        Me.txt_id_combo.Location = New System.Drawing.Point(125, 36)
         Me.txt_id_combo.Name = "txt_id_combo"
         Me.txt_id_combo.Size = New System.Drawing.Size(100, 20)
         Me.txt_id_combo.TabIndex = 24
         '
-        'lbl_precio
-        '
-        Me.lbl_precio.AutoSize = True
-        Me.lbl_precio.Location = New System.Drawing.Point(24, 103)
-        Me.lbl_precio.Name = "lbl_precio"
-        Me.lbl_precio.Size = New System.Drawing.Size(68, 13)
-        Me.lbl_precio.TabIndex = 21
-        Me.lbl_precio.Text = "Precio Venta"
-        '
         'lbl_nombre_combo
         '
         Me.lbl_nombre_combo.AutoSize = True
-        Me.lbl_nombre_combo.Location = New System.Drawing.Point(24, 67)
+        Me.lbl_nombre_combo.Location = New System.Drawing.Point(24, 74)
         Me.lbl_nombre_combo.Name = "lbl_nombre_combo"
         Me.lbl_nombre_combo.Size = New System.Drawing.Size(80, 13)
         Me.lbl_nombre_combo.TabIndex = 20
@@ -178,7 +151,7 @@ Partial Class AbmCombo
         'lbl_id_combo
         '
         Me.lbl_id_combo.AutoSize = True
-        Me.lbl_id_combo.Location = New System.Drawing.Point(24, 32)
+        Me.lbl_id_combo.Location = New System.Drawing.Point(24, 39)
         Me.lbl_id_combo.Name = "lbl_id_combo"
         Me.lbl_id_combo.Size = New System.Drawing.Size(54, 13)
         Me.lbl_id_combo.TabIndex = 19
@@ -186,7 +159,7 @@ Partial Class AbmCombo
         '
         'btn_agregar_articulo
         '
-        Me.btn_agregar_articulo.Location = New System.Drawing.Point(29, 386)
+        Me.btn_agregar_articulo.Location = New System.Drawing.Point(29, 393)
         Me.btn_agregar_articulo.Name = "btn_agregar_articulo"
         Me.btn_agregar_articulo.Size = New System.Drawing.Size(90, 23)
         Me.btn_agregar_articulo.TabIndex = 31
@@ -195,7 +168,7 @@ Partial Class AbmCombo
         '
         'btn_quitar_articulo
         '
-        Me.btn_quitar_articulo.Location = New System.Drawing.Point(138, 386)
+        Me.btn_quitar_articulo.Location = New System.Drawing.Point(138, 393)
         Me.btn_quitar_articulo.Name = "btn_quitar_articulo"
         Me.btn_quitar_articulo.Size = New System.Drawing.Size(78, 23)
         Me.btn_quitar_articulo.TabIndex = 32
@@ -204,37 +177,39 @@ Partial Class AbmCombo
         '
         'btn_editar_combo
         '
-        Me.btn_editar_combo.Location = New System.Drawing.Point(409, 386)
+        Me.btn_editar_combo.Location = New System.Drawing.Point(409, 393)
         Me.btn_editar_combo.Name = "btn_editar_combo"
         Me.btn_editar_combo.Size = New System.Drawing.Size(81, 23)
         Me.btn_editar_combo.TabIndex = 33
         Me.btn_editar_combo.Text = "Editar Combo"
         Me.btn_editar_combo.UseVisualStyleBackColor = True
         '
-        'txt_precio_sugerido
+        'MenuStrip1
         '
-        Me.txt_precio_sugerido.Enabled = False
-        Me.txt_precio_sugerido.Location = New System.Drawing.Point(390, 100)
-        Me.txt_precio_sugerido.Name = "txt_precio_sugerido"
-        Me.txt_precio_sugerido.Size = New System.Drawing.Size(100, 20)
-        Me.txt_precio_sugerido.TabIndex = 35
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.BuscarToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(517, 24)
+        Me.MenuStrip1.TabIndex = 36
+        Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'lbl_precio_sugerido
+        'NuevoToolStripMenuItem
         '
-        Me.lbl_precio_sugerido.AutoSize = True
-        Me.lbl_precio_sugerido.Location = New System.Drawing.Point(289, 103)
-        Me.lbl_precio_sugerido.Name = "lbl_precio_sugerido"
-        Me.lbl_precio_sugerido.Size = New System.Drawing.Size(82, 13)
-        Me.lbl_precio_sugerido.TabIndex = 34
-        Me.lbl_precio_sugerido.Text = "Precio Sugerido"
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'BuscarToolStripMenuItem
+        '
+        Me.BuscarToolStripMenuItem.Name = "BuscarToolStripMenuItem"
+        Me.BuscarToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.BuscarToolStripMenuItem.Text = "Buscar"
         '
         'AbmCombo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(517, 490)
-        Me.Controls.Add(Me.txt_precio_sugerido)
-        Me.Controls.Add(Me.lbl_precio_sugerido)
         Me.Controls.Add(Me.btn_editar_combo)
         Me.Controls.Add(Me.btn_quitar_articulo)
         Me.Controls.Add(Me.btn_agregar_articulo)
@@ -243,16 +218,17 @@ Partial Class AbmCombo
         Me.Controls.Add(Me.btn_salir)
         Me.Controls.Add(Me.btn_guardar)
         Me.Controls.Add(Me.btn_eliminar)
-        Me.Controls.Add(Me.btn_nuevo)
-        Me.Controls.Add(Me.txt_precio)
         Me.Controls.Add(Me.txt_nombre_articulo)
         Me.Controls.Add(Me.txt_id_combo)
-        Me.Controls.Add(Me.lbl_precio)
         Me.Controls.Add(Me.lbl_nombre_combo)
         Me.Controls.Add(Me.lbl_id_combo)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "AbmCombo"
         Me.Text = "AbmCombo"
         CType(Me.dgv_datos_articulos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -263,11 +239,8 @@ Partial Class AbmCombo
     Friend WithEvents btn_salir As Button
     Friend WithEvents btn_guardar As Button
     Friend WithEvents btn_eliminar As Button
-    Friend WithEvents btn_nuevo As Button
-    Friend WithEvents txt_precio As TextBox
     Friend WithEvents txt_nombre_articulo As TextBox
     Friend WithEvents txt_id_combo As TextBox
-    Friend WithEvents lbl_precio As Label
     Friend WithEvents lbl_nombre_combo As Label
     Friend WithEvents lbl_id_combo As Label
     Friend WithEvents c_id_articulo As DataGridViewTextBoxColumn
@@ -277,6 +250,7 @@ Partial Class AbmCombo
     Friend WithEvents btn_agregar_articulo As Button
     Friend WithEvents btn_quitar_articulo As Button
     Friend WithEvents btn_editar_combo As Button
-    Friend WithEvents txt_precio_sugerido As TextBox
-    Friend WithEvents lbl_precio_sugerido As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BuscarToolStripMenuItem As ToolStripMenuItem
 End Class
--- PAVTPI/PAVTPI/GUILayer/AbmCombo.vb
+++ PAVTPI/PAVTPI/GUILayer/AbmCombo.vb
@@ -14,27 +14,60 @@
         txt_nombre_articulo.Enabled = True
         txt_nombre_articulo.Text = Nothing
 
-        txt_precio.Enabled = True
-        txt_precio.Text = Nothing
-
-        txt_precio_sugerido.Enabled = False
-        txt_precio_sugerido.Text = Nothing
-
-
-        btn_agregar_articulo.Enabled = True
-        btn_quitar_articulo.Enabled = True
-
-        btn_nuevo.Enabled = True
         btn_guardar.Enabled = True
-        btn_buscar.Enabled = True
+        btn_buscar.Enabled = False
+        btn_buscar.Visible = False
         btn_eliminar.Enabled = True
         btn_salir.Enabled = True
+
+        btn_agregar_articulo.Enabled = True
+        btn_agregar_articulo.Visible = True
+        btn_quitar_articulo.Enabled = True
+        btn_quitar_articulo.Visible = True
 
         btn_editar_combo.Enabled = False
         btn_editar_combo.Visible = False
     End Sub
 
     Private Sub setBuscar()
+        txt_id_combo.Enabled = True
+        txt_id_combo.Text = Nothing
+
+        txt_nombre_articulo.Enabled = False
+        txt_nombre_articulo.Text = Nothing
+
+        btn_guardar.Enabled = False
+        btn_buscar.Enabled = True
+        btn_buscar.Visible = True
+        btn_eliminar.Enabled = True
+        btn_salir.Enabled = True
+
+        btn_agregar_articulo.Enabled = False
+        btn_agregar_articulo.Visible = False
+        btn_quitar_articulo.Enabled = False
+        btn_quitar_articulo.Visible = False
+
+        btn_editar_combo.Enabled = False
+        btn_editar_combo.Visible = True
+    End Sub
+
+    Private Sub setEdit()
+        txt_id_combo.Enabled = False
+        txt_nombre_articulo.Enabled = True
+
+        btn_buscar.Enabled = False
+        btn_buscar.Visible = False
+
+        btn_guardar.Enabled = True
+        btn_eliminar.Enabled = True
+        btn_salir.Enabled = True
+        btn_agregar_articulo.Enabled = True
+        btn_agregar_articulo.Visible = True
+        btn_quitar_articulo.Enabled = True
+        btn_quitar_articulo.Visible = True
+
+        btn_editar_combo.Visible = False
+        btn_editar_combo.Enabled = False
 
     End Sub
 
@@ -50,4 +83,12 @@
         Next
         Return ret
     End Function
+
+    Private Sub NuevoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NuevoToolStripMenuItem.Click
+        setNuevo()
+    End Sub
+
+    Private Sub BuscarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BuscarToolStripMenuItem.Click
+        setBuscar()
+    End Sub
 End Class
