79fcf4c0e7c22f0c7e9acdcdc9fa8053622ccea7
levicastro36 <levicastro36@gmail.com>
2017-05-18 20:14:11 -0300

    dsa

--- PAVTPI/PAVTPI/GUILayer/ArticulosParaCombo.vb
+++ PAVTPI/PAVTPI/GUILayer/ArticulosParaCombo.vb
@@ -11,6 +11,7 @@
         Dim strSQL As String = "SELECT * FROM articulo"
         For Each row As DataRow In DBHelper.getDBHelper().ConsultaSQL(strSQL).Rows
             Me.dgv_datos_articulos.Rows.Add(row)
+            'fafsa
         Next
     End Sub
 
