05c3e76ac106819d0cb37864989700a82d110990
Matias <Matias@LAPTOP-6VOLNCDP>
2017-06-14 00:26:17 -0300

    me fui a dormir

--- PAVTPI/PAVTPI/GUILayer/Resumen Cta Cte.Designer.vb
+++ PAVTPI/PAVTPI/GUILayer/Resumen Cta Cte.Designer.vb
@@ -42,7 +42,7 @@ Partial Class Resumen_Cta_Cte
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "PAVTPI.ReporteCtaCte.rdlc"
         Me.ReportViewer1.Location = New System.Drawing.Point(12, 34)
         Me.ReportViewer1.Name = "ReportViewer1"
-        Me.ReportViewer1.Size = New System.Drawing.Size(673, 478)
+        Me.ReportViewer1.Size = New System.Drawing.Size(1208, 478)
         Me.ReportViewer1.TabIndex = 0
         '
         'Label1
@@ -84,7 +84,7 @@ Partial Class Resumen_Cta_Cte
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(697, 524)
+        Me.ClientSize = New System.Drawing.Size(1232, 524)
         Me.Controls.Add(Me.btnBuscar)
         Me.Controls.Add(Me.txtCtaCte)
         Me.Controls.Add(Me.Label1)
