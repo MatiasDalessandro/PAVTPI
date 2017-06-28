0bcfccc79e9f2bc68e8ecc3bb807fde6b99425cc
Alvaro <Alvaro@ALVARO-PC>
2017-06-28 13:30:26 -0300

    sesf

--- PAVTPI/PAVTPI/GUILayer/Estadística/ArticuloMvendido.Designer.vb
+++ PAVTPI/PAVTPI/GUILayer/Estadística/ArticuloMvendido.Designer.vb
@@ -23,23 +23,24 @@ Partial Class ArticuloMvendido
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ArticuloMVendidoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.EstadisticasDependencias = New PAVTPI.EstadisticasDependencias()
-        Me.ArticuloMVendidoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        CType(Me.EstadisticasDependencias, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btn_amv = New System.Windows.Forms.Button()
         CType(Me.ArticuloMVendidoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EstadisticasDependencias, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ReportViewer1
         '
-        ReportDataSource1.Name = "DataSet1"
-        ReportDataSource1.Value = Me.ArticuloMVendidoBindingSource
-        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        ReportDataSource2.Name = "DataSet1"
+        ReportDataSource2.Value = Me.ArticuloMVendidoBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "PAVTPI.ArticuloMasVendido.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(12, 12)
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 75)
         Me.ReportViewer1.Name = "ReportViewer1"
-        Me.ReportViewer1.Size = New System.Drawing.Size(1043, 561)
+        Me.ReportViewer1.Size = New System.Drawing.Size(1043, 498)
         Me.ReportViewer1.TabIndex = 0
         '
         'EstadisticasDependencias
@@ -47,16 +48,26 @@ Partial Class ArticuloMvendido
         Me.EstadisticasDependencias.DataSetName = "EstadisticasDependencias"
         Me.EstadisticasDependencias.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
+        'btn_amv
+        '
+        Me.btn_amv.Location = New System.Drawing.Point(23, 23)
+        Me.btn_amv.Name = "btn_amv"
+        Me.btn_amv.Size = New System.Drawing.Size(75, 23)
+        Me.btn_amv.TabIndex = 1
+        Me.btn_amv.Text = "Generar"
+        Me.btn_amv.UseVisualStyleBackColor = True
+        '
         'ArticuloMvendido
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1067, 585)
+        Me.Controls.Add(Me.btn_amv)
         Me.Controls.Add(Me.ReportViewer1)
         Me.Name = "ArticuloMvendido"
         Me.Text = "ArticuloMvendido"
-        CType(Me.EstadisticasDependencias, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.ArticuloMVendidoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EstadisticasDependencias, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -64,4 +75,5 @@ Partial Class ArticuloMvendido
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
     Friend WithEvents EstadisticasDependencias As EstadisticasDependencias
     Friend WithEvents ArticuloMVendidoBindingSource As BindingSource
+    Friend WithEvents btn_amv As Button
 End Class
--- PAVTPI/PAVTPI/GUILayer/Estadística/ArticuloMvendido.vb
+++ PAVTPI/PAVTPI/GUILayer/Estadística/ArticuloMvendido.vb
@@ -1,6 +1,26 @@
 ﻿Public Class ArticuloMvendido
+    Dim dbhelper As DBHelper = DBHelper.getDBHelper
     Private Sub ArticuloMvendido_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub generar_articuloMasVendido()
+        Dim tabla As New DataTable
+        Dim SQL As String = ""
+
+        SQL &= " select A.nombre , DTA.cantidad"
+        SQL &= "        from articulo A"
+        SQL &= "        join detalleTicketArticulo DTA on DTA.idArticulo = A.idArticuloIntegrante "
+
+        tabla = dbhelper.ConsultaSQL(SQL)
+
+        ArticuloMVendidoBindingSource.DataSource = tabla
+        ReportViewer1.RefreshReport()
+
+    End Sub
+
+    Private Sub btn_amv_Click(sender As Object, e As EventArgs) Handles btn_amv.Click
+        generar_articuloMasVendido()
     End Sub
 End Class
